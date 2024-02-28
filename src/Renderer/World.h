5524032cb50c879546f2e51616bb16a55b765b25
Mikhail Getmanov <mmgetmanov@gmail.com>
2024-02-28 22:12:32 +0300

    Declared basic interface for World class

--- src/Renderer/World.h
+++ src/Renderer/World.h
@@ -1,6 +1,31 @@
+#include <vector>
+
+#include "Camera.h"
+#include "TriangularObject.h"
+
 namespace ThreeDRenderer {
 class World {
+    using Object = TriangularObject;
+
 public:
+    World();
+
+    void AddObject(Object obj);
+
+    void CamMoveForward();
+    void CamMoveBackwads();
+    void CamMoveLeft();
+    void CamMoveRight();
+    void CamRotateUp();
+    void CamRotateDown();
+    void CamRotateLeft();
+    void CamRotateRight();
+
+    const std::vector<Object> GetObjects() const;
+    const Camera& GetCamera() const;
+
 private:
+    std::vector<Object> objects_;
+    Camera cam_;
 };
 }  // namespace ThreeDRenderer
