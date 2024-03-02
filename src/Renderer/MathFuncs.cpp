93a394669e3a6bd05e3e595a1b8e6b8014449d69
Mikhail Getmanov <mmgetmanov@gmail.com>
2024-03-02 15:23:30 +0300

    Added file for custom math functions

---
+++ src/Renderer/MathFuncs.cpp
@@ -0,0 +1,8 @@
+#include "MathFuncs.h"
+
+namespace ThreeDRenderer {
+double DegToRad(double angle) {
+    return angle * M_PI / 180;
+}
+
+}  // namespace ThreeDRenderer
---
+++ src/Renderer/MathFuncs.h
@@ -0,0 +1,9 @@
+#pragma once
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <numbers>
+
+namespace ThreeDRenderer {
+double DegToRad(double angle);
+
+}  // namespace ThreeDRenderer
