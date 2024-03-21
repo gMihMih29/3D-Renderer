7e14ba9c14d80035325b6266cdb4dea5b6f67b1e
Mikhail Getmanov <mmgetmanov@gmail.com>
2024-03-21 21:37:58 +0300

    Added some definitions for TriangularObject class

--- src/Renderer/TriangularObject.cpp
+++ src/Renderer/TriangularObject.cpp
@@ -1,1 +1,69 @@
 #include "TriangularObject.h"
+
+namespace ThreeDRenderer {
+TriangularObject::TriangularObject(CoordinatesVector pos, ColorVector color,
+                                   const std::vector<CoordinatesVector>& vertexes,
+                                   const std::vector<ConnectionVector>& connections) {
+    SetPosition(pos);
+    connections_ = connections;
+    color_ = color;
+    for (int i = 0; i < 3; ++i) {
+        while (color_(i) < 0) {
+            color_(i) += 256;
+        }
+        color(i) %= 256;
+    }
+    vertexes_.resize(4, vertexes.size());
+    for (int i = 0; i < vertexes.size(); ++i) {
+        vertexes_(0, i) = vertexes[i](0);
+        vertexes_(1, i) = vertexes[i](1);
+        vertexes_(2, i) = vertexes[i](2);
+        vertexes_(3, i) = 1;
+    }
+    for (int i = 0; i < connections_.size(); ++i) {
+        while (connections_[i](0) < 0) {
+            connections_[i](0) += vertexes_.cols();
+        }
+        connections_[i](0) %= vertexes_.cols();
+        while (connections_[i](1) < 0) {
+            connections_[i](1) += vertexes_.cols();
+        }
+        connections_[i](1) %= vertexes_.cols();
+        while (connections_[i](2) < 0) {
+            connections_[i](2) += vertexes_.cols();
+        }
+        connections_[i](2) %= vertexes_.cols();
+    }
+}
+
+void TriangularObject::SetPosition(const CoordinatesVector& pos) {
+    position_(0) = pos(0);
+    position_(1) = pos(1);
+    position_(2) = pos(2);
+    position_(3) = 0;
+}
+
+TriangularObject::CoordinatesVector TriangularObject::GetPosition() const {
+    return CoordinatesVector(position_(0), position_(1), position_(2));
+}
+
+const TriangularObject::ColorVector& TriangularObject::GetColor() const {
+    return color_;
+}
+
+const TriangularObject::CoordinatesMatrix& TriangularObject::GetVertexesLocal() const {
+    return vertexes_;
+}
+
+const std::vector<TriangularObject::ConnectionVector>& TriangularObject::GetConnections() const {
+    return connections_;
+}
+
+TriangularObject::CoordinatesMatrix TriangularObject::GetVertexesGlobal() const {
+    CoordinatesMatrix res = vertexes_;
+    for (int i = 0; i < res.cols(); ++i) {
+        res.col(i) += position_;
+    }
+    return res;
+}
+}  // namespace ThreeDRenderer
--- src/Renderer/TriangularObject.h
+++ src/Renderer/TriangularObject.h
@@ -6,27 +6,26 @@
 namespace ThreeDRenderer {
 class TriangularObject {
     using CoordinatesVector = Eigen::Vector3d;
+    using CoordinatesVector4D = Eigen::Vector4d;
     using ConnectionVector = Eigen::Vector3i;
     using ColorVector = Eigen::Vector3i;
     using CoordinatesMatrix = Eigen::MatrixXd;
 
 public:
-    TriangularObject();
+    TriangularObject() = delete;
     TriangularObject(CoordinatesVector pos, ColorVector color, const std::vector<CoordinatesVector>& vertexes,
                      const std::vector<ConnectionVector>& connections);
 
     void SetPosition(const CoordinatesVector& pos);
-    void AddVertex(const CoordinatesVector& pos);
-    void AddConnection(const ConnectionVector& connection);
 
-    const CoordinatesVector& GetPosition() const;
+    CoordinatesVector GetPosition() const;
     const ColorVector& GetColor() const;
     const CoordinatesMatrix& GetVertexesLocal() const;
     CoordinatesMatrix GetVertexesGlobal() const;
     const std::vector<ConnectionVector>& GetConnections() const;
 
 private:
-    CoordinatesVector position_ = CoordinatesVector(0, 0, 0);
+    CoordinatesVector4D position_ = CoordinatesVector4D(0, 0, 0, 0);
     ColorVector color_ = ColorVector(0, 0, 0);
     CoordinatesMatrix vertexes_;
     std::vector<ConnectionVector> connections_;
