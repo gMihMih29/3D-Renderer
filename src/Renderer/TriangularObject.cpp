0ffe19b6fa65a1bf0e62aa6c864dd2dfe02254bb
Mikhail Getmanov <mmgetmanov@gmail.com>
2024-03-24 13:26:23 +0300

    Changed Triangle object structure

--- src/Renderer/TriangularObject.cpp
+++ src/Renderer/TriangularObject.cpp
@@ -1,38 +1,67 @@
 #include "TriangularObject.h"
 
+#include <cassert>
+#include <cmath>
+
+#include "../Utilities/Logger.h"
+
 namespace ThreeDRenderer {
+
+TriangularObject::TriangularObject(CoordinatesVector pos, ColorVector color, std::vector<Surface>&& surfaces) {
+    position_ = CoordinatesVector4D(pos(0), pos(1), pos(2), 0);
+    color_ = color;
+    surfaces_ = surfaces;
+}
+
 TriangularObject::TriangularObject(CoordinatesVector pos, ColorVector color,
                                    const std::vector<CoordinatesVector>& vertexes,
                                    const std::vector<ConnectionVector>& connections) {
-    SetPosition(pos);
-    connections_ = connections;
+    assert(vertexes.size() != 0 && "There must be some vertexes for object");
+    assert(connections.size() != 0 && "There must be some connections for object");
+    position_ = CoordinatesVector4D(pos(0), pos(1), pos(2), 0);
     color_ = color;
-    for (int i = 0; i < 3; ++i) {
-        while (color_(i) < 0) {
-            color_(i) += 256;
+    size_t cntVertexes = vertexes.size();
+    std::vector<NormalVector> normals(cntVertexes);
+    for (int i = 0; i < cntVertexes; ++i) {
+        std::vector<bool> used(cntVertexes);
+        for (int j = 0; j < connections.size(); ++j) {
+            assert(connections[j].size() >= 3 && "Surfaces must contain at least 3 vertexes");
+            for (int k = 0; k < connections[j].size(); ++k) {
+                if (connections[j][k] == i) {
+                    int prev = (k - 1) % connections[j].size();
+                    if (prev < 0) {
+                        prev += connections[j].size();
+                    }
+                    assert(0 <= prev && prev < connections[j].size() && "prev has incorrect index");
+                    int next = (k + 1) % connections[j].size();
+                    if (!used[prev]) {
+                        normals[i] += vertexes[connections[j][prev]] - vertexes[i];
+                        used[prev] = true;
+                    }
+                    if (!used[next]) {
+                        normals[i] += vertexes[connections[j][next]] - vertexes[i];
+                        used[next] = true;
+                    }
+                }
+            }
         }
-        color(i) %= 256;
     }
-    vertexes_.resize(4, vertexes.size());
-    for (int i = 0; i < vertexes.size(); ++i) {
-        vertexes_(0, i) = vertexes[i](0);
-        vertexes_(1, i) = vertexes[i](1);
-        vertexes_(2, i) = vertexes[i](2);
-        vertexes_(3, i) = 1;
+    for (int i = 0; i < cntVertexes; ++i) {
+        assert(!normals[i].isZero() && "Normal vector must be non zero");
+        normals[i] = -normals[i];
+        // normals[i].normalize();
     }
-    for (int i = 0; i < connections_.size(); ++i) {
-        while (connections_[i](0) < 0) {
-            connections_[i](0) += vertexes_.cols();
+    for (int i = 0; i < connections.size(); ++i) {
+        NormalVector surfaceNorm;
+        for (int j = 0; j < connections[i].size(); ++j) {
+            surfaceNorm += normals[connections[i][j]];
         }
-        connections_[i](0) %= vertexes_.cols();
-        while (connections_[i](1) < 0) {
-            connections_[i](1) += vertexes_.cols();
+        assert(!surfaceNorm.isZero() && "Normal vector must be non zero");
+        surfaceNorm.normalize();
+        for (int j = 1; j + 1 < connections[i].size(); ++j) {
+            surfaces_.emplace_back(vertexes[connections[i][0]], vertexes[connections[i][j]],
+                                   vertexes[connections[i][j + 1]], surfaceNorm);
         }
-        connections_[i](1) %= vertexes_.cols();
-        while (connections_[i](2) < 0) {
-            connections_[i](2) += vertexes_.cols();
-        }
-        connections_[i](2) %= vertexes_.cols();
     }
 }
 
@@ -51,19 +80,38 @@ const TriangularObject::ColorVector& TriangularObject::GetColor() const {
     return color_;
 }
 
-const TriangularObject::CoordinatesMatrix& TriangularObject::GetVertexesLocal() const {
-    return vertexes_;
-}
-
-const std::vector<TriangularObject::ConnectionVector>& TriangularObject::GetConnections() const {
-    return connections_;
-}
-
-TriangularObject::CoordinatesMatrix TriangularObject::GetVertexesGlobal() const {
-    CoordinatesMatrix res = vertexes_;
-    for (int i = 0; i < res.cols(); ++i) {
-        res.col(i) += position_;
+TriangularObject::CoordinatesMatrix TriangularObject::GetVertexesLocal() const {
+    CoordinatesMatrix res;
+    res.resize(4, 3 * surfaces_.size());
+    for (int i = 0; i < surfaces_.size(); ++i) {
+        res.col(3 * i) = surfaces_[i].GetPoint(0);
+        res.col(3 * i + 1) = surfaces_[i].GetPoint(1);
+        res.col(3 * i + 2) = surfaces_[i].GetPoint(2);
     }
     return res;
 }
+
+TriangularObject::CoordinatesMatrix TriangularObject::GetVertexesGlobal() const {
+    CoordinatesMatrix res;
+    res.resize(4, 3 * surfaces_.size());
+    for (int i = 0; i < surfaces_.size(); ++i) {
+        res.col(3 * i) = surfaces_[i].GetPoint(0) + position_;
+        res.col(3 * i + 1) = surfaces_[i].GetPoint(1) + position_;
+        res.col(3 * i + 2) = surfaces_[i].GetPoint(2) + position_;
+    }
+    return res;
+}
+
+std::vector<TriangularObject::NormalVector> TriangularObject::GetNormalVectors() const {
+    std::vector<NormalVector> res(surfaces_.size());
+    for (int i = 0; i < res.size(); ++i) {
+        res[i] = surfaces_[i].GetNormalVector();
+    }
+    return res;
+}
+
+const std::vector<TriangularObject::Surface>& TriangularObject::GetSurfaces() const {
+    return surfaces_;
+}
+
 }  // namespace ThreeDRenderer
--- src/Renderer/TriangularObject.h
+++ src/Renderer/TriangularObject.h
@@ -1,33 +1,39 @@
 #pragma once
 
 #include <Eigen/Dense>
+#include <SFML/Graphics/Color.hpp>
+#include <utility>
 #include <vector>
+
+#include "Triangle.h"
 
 namespace ThreeDRenderer {
 class TriangularObject {
+public:
+    using ColorVector = sf::Color;
+    using ConnectionVector = std::vector<int>;
+    using CoordinatesMatrix = Eigen::Matrix4Xd;
     using CoordinatesVector = Eigen::Vector3d;
     using CoordinatesVector4D = Eigen::Vector4d;
-    using ConnectionVector = Eigen::Vector3i;
-    using ColorVector = Eigen::Vector3i;
-    using CoordinatesMatrix = Eigen::MatrixXd;
+    using NormalVector = Eigen::Vector3d;
+    using Surface = Triangle;
 
 public:
-    TriangularObject() = delete;
+    TriangularObject(CoordinatesVector pos, ColorVector color, std::vector<Surface>&& surfaces);
     TriangularObject(CoordinatesVector pos, ColorVector color, const std::vector<CoordinatesVector>& vertexes,
                      const std::vector<ConnectionVector>& connections);
 
     void SetPosition(const CoordinatesVector& pos);
-
     CoordinatesVector GetPosition() const;
     const ColorVector& GetColor() const;
-    const CoordinatesMatrix& GetVertexesLocal() const;
+    CoordinatesMatrix GetVertexesLocal() const;
     CoordinatesMatrix GetVertexesGlobal() const;
-    const std::vector<ConnectionVector>& GetConnections() const;
+    std::vector<NormalVector> GetNormalVectors() const;
+    const std::vector<Surface>& GetSurfaces() const;
 
 private:
     CoordinatesVector4D position_ = CoordinatesVector4D(0, 0, 0, 0);
     ColorVector color_ = ColorVector(0, 0, 0);
-    CoordinatesMatrix vertexes_;
-    std::vector<ConnectionVector> connections_;
+    std::vector<Surface> surfaces_;
 };
 }  // namespace ThreeDRenderer
