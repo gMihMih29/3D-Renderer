157119d6810cd45f2c50461c9f176f7f52c92ea6
Mikhail Getmanov <mmgetmanov@gmail.com>
2024-03-29 16:34:12 +0300

    Added light in world class

--- src/Renderer/World/World.cpp
+++ src/Renderer/World/World.cpp
@@ -6,8 +6,24 @@ void World::AddObject(World::Object&& obj) {
     objects_.push_back(std::move(obj));
 }
 
+void World::AddDirectionalLight(DirectionalLight light) {
+    directional_light_sources_.push_back(light);
+}
+
+void World::SetAmbientLight(AmbientLight light) {
+    ambient_light_ = light;
+}
+
 const std::vector<World::Object>& World::GetObjects() const {
     return objects_;
+}
+
+const std::vector<DirectionalLight>& World::GetDirectionalLight() const {
+    return directional_light_sources_;
+}
+
+const AmbientLight& World::GetAmbientLight() const {
+    return ambient_light_;
 }
 
 }  // namespace ThreeDRenderer
--- src/Renderer/World/World.h
+++ src/Renderer/World/World.h
@@ -4,6 +4,8 @@
 
 #include "Camera.h"
 #include "../Primitives/TriangularObject.h"
+#include "../Light/AmbientLight.h"
+#include "../Light/DirectionalLight.h"
 
 namespace ThreeDRenderer {
 
@@ -14,10 +16,16 @@ public:
     World() = default;
 
     void AddObject(Object&& obj);
+    void AddDirectionalLight(DirectionalLight light);
+    void SetAmbientLight(AmbientLight light);
     const std::vector<Object>& GetObjects() const;
+    const std::vector<DirectionalLight>& GetDirectionalLight() const;
+    const AmbientLight& GetAmbientLight() const;
 
 private:
     std::vector<Object> objects_;
+    std::vector<DirectionalLight> directional_light_sources_;
+    AmbientLight ambient_light_;
 };
 
 }  // namespace ThreeDRenderer
