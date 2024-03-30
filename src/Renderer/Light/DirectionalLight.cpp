1d17816cb42e8485341b7054ba39d57517449222
Mikhail Getmanov <mmgetmanov@gmail.com>
2024-03-30 19:45:19 +0300

    Changed constructor

--- src/Renderer/Light/DirectionalLight.cpp
+++ src/Renderer/Light/DirectionalLight.cpp
@@ -1,10 +1,12 @@
 #include "DirectionalLight.h"
-#include "../Funcs/MathFuncs.h"
 
 #include <cassert>
 
+#include "../Funcs/MathFuncs.h"
+
 namespace ThreeDRenderer {
-DirectionalLight::DirectionalLight(Vector4 dir, sf::Color color) : direction_(dir), color_(color) {
+DirectionalLight::DirectionalLight(Vector3 dir, sf::Color color)
+    : direction_(dir(0), dir(1), dir(2), 0), color_(color) {
     assert(!direction_.isZero() && "Direction of light must be non zero!");
     direction_.normalize();
 }
--- src/Renderer/Light/DirectionalLight.h
+++ src/Renderer/Light/DirectionalLight.h
@@ -7,10 +7,11 @@
 
 namespace ThreeDRenderer {
 class DirectionalLight {
+    using Vector3 = Eigen::Vector3d;
     using Vector4 = Eigen::Vector4d;
 
 public:
-    DirectionalLight(Vector4 dir, sf::Color color);
+    DirectionalLight(Vector3 dir, sf::Color color);
 
     const Vector4& GetDirection() const;
     const Color& GetColor() const;
